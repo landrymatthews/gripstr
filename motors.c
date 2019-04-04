76fbd64bc7b6b5188feecd8217ae0b3164e14690
landrymatthews <33405055+landrymatthews@users.noreply.github.com>
2019-04-04 15:08:19 -0500

    Update motors.c

--- motors.c
+++ motors.c
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
 Servo myservo;
-
+int flexiForcePin = 0;
 void setup() {
 	Serial.begin(9600);
 	myservo.attach(9);
@@ -9,11 +9,15 @@ void setup() {
 }
 
 void loop() {
-	int currentAngle = 0;
-	myservo.write(currentAngle);
-	Serial.println(currentAngle);
-	currentAngle = 90;
-	myservo.write(currentAngle);
-	serial.println(currentAngle);
-	delay(1000);
+	int current_sensor_value = analogRead(flexiForcePin);
+  	//Serial.println(flexiForceReading);
+	# normalizing 0-1024 from the sensor to 0-60 degrees for the servo
+	int min_sensor_value = 0
+	int max_sensor_value = 1024
+	int min_angle = 0
+	int max_angle = 60
+	int servo_angle = (max_angle-min_angle)*(current_sensor_value / max_sensor_value) + min_angle;
+
+	myservo.write(servo_angle);
+	Serial.println(servo_angle);
 }
